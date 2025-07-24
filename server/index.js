473874fb3fe49cdf32902dad5b931956f5de88d4
HM18042005 <harshit04185@gmail.com>
2025-07-24 17:20:41 +0530

    base start

---
+++ server/index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+require('dotenv').config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+// MongoDB connection
+mongoose.connect(process.env.MONGO_URI ,{ useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('Connected to MongoDB'))
+  .catch((error) => console.error('MongoDB connection error:', error));
+
+// Test route
+app.get('/', (req, res) => res.send('Hello from backend!'));
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
