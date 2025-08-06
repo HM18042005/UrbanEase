28d04d061405c31398b5b16bbaf02b2e36677d90
HM18042005 <harshit04185@gmail.com>
2025-08-06 23:29:07 +0530

    frontend

---
+++ server/models/user.js
@@ -0,0 +1,8 @@
+const mongoose = require('mongoose');
+const UserSchema = new mongoose.Schema({
+  name: String,
+  email: String,
+  password: String, // hash in real use
+  role: { type: String, enum: ['customer', 'provider', 'admin'] }
+});
+module.exports = mongoose.model('User', UserSchema);
